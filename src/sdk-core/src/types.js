b43dd8781e582f0f2d959080def80a00a08b63e3
alex <alexxxkny@mail.ru>
2022-09-15 22:58:13 +0300

    ignore *.js

--- src/sdk-core/src/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TokenType = void 0;
-var TokenType;
-(function (TokenType) {
-    TokenType[TokenType["BASIC"] = 0] = "BASIC";
-    TokenType[TokenType["DEX_SHARE"] = 1] = "DEX_SHARE";
-})(TokenType = exports.TokenType || (exports.TokenType = {}));
