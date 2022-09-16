dfff8c69efbe3d163aeb5aa3cb47ee0eb35b6448
alex <alexxxkny@mail.ru>
2022-09-16 21:31:54 +0300

    example exact_target_swap added

---
+++ src/examples/exact_target_swap.ts
@@ -0,0 +1,75 @@
+import { getApiPromise, getSwapPromiseFromApi, getDefaultAccountsKeyring } from './common';
+import { FixedPointNumber, Token, TokenPair } from '../sdk-core/src';
+ 
+async function main() {
+    const api = await getApiPromise();
+
+    const swapPromise = getSwapPromiseFromApi(api);
+
+    // let we have CGT-DOT token pair
+    const CGT = Token.fromCurrencyName('CGT', { decimals: 18} );
+    const DOT = Token.fromCurrencyName('DOT', { decimals: 10} );
+
+    // user selected input and output tokens
+    const inputToken = DOT;
+    const outputToken = CGT;
+
+    // user entered the desired output amount
+
+    // !!! Precision (second arg) should be equal to token decimals if the value will be used as token amount !!!
+    const outputAmount = new FixedPointNumber(100, outputToken.decimals);  
+
+    // calculate swap parameters
+    const swapParams = await swapPromise.swap([inputToken, outputToken], outputAmount, 'EXACT_OUTPUT');
+
+    // i guess there will not be this case because we will only have enabled trading pairs
+    if (!swapParams) {
+        console.log('unable to swap');
+        return;
+    }
+
+    // Show info for the user 
+    // Especially priceImpact and output
+    console.log(`midPrice: ${swapParams?.midPrice}`);
+    console.log(`priceImpact: ${swapParams?.priceImpact}`);
+    console.log(`naturalPriceImpact: ${swapParams?.naturalPriceImpact}`);
+    console.log(`input: ${swapParams?.input.token.name} ${swapParams?.input.balance}`);
+    console.log(`output: ${swapParams?.output.token.name} ${swapParams?.output.balance}`);
+    console.log(`exchangeFee: ${swapParams?.exchangeFee}`);
+    console.log(`exchangeRate: ${swapParams?.exchangeRate}`);
+
+    // if user didn't specify the other one
+    const maxInputAmount = swapParams.input.balance; 
+
+    // Example account
+    const [alice, ] = getDefaultAccountsKeyring();
+
+    // Chain data
+    console.log('Chain data');
+    console.log(`Input token: ${swapParams.input.token.toChainData()}`);
+    console.log(`Output token: ${swapParams.output.token.toChainData()}`);
+    console.log(`Input amount: ${swapParams.input.balance.toChainData()}`);
+    console.log(`Maximum supply amount: ${maxInputAmount.toChainData()}`);
+
+    //Make a swap after user vconfirmation
+    const unsub = await api.tx.dex
+        .swapWithExactTarget(  
+            [
+                swapParams.input.token.toChainData(),
+                swapParams.output.token.toChainData()
+            ], 
+            swapParams.input.balance.toChainData(), 
+            maxInputAmount.toChainData()
+        )
+        .signAndSend(alice, (result) => {
+            console.log(`Current status is ${result.status}`);
+            if (result.status.isInBlock) {
+            console.log(`Transaction included at blockHash ${result.status.asInBlock}`);
+            } else if (result.status.isFinalized) {
+            console.log(`Transaction finalized at blockHash ${result.status.asFinalized}`);
+            unsub();
+            }
+        });
+}
+
+main();
