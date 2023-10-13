f142d2b9680739d821d276e392d32582abeb5e79
Victoria Kapitonenko <v.kapitonenko@rebase.agency>
2023-10-13 12:14:20 +0300

    fix: exports from js files

--- src/interfaces/definitions.ts
+++ src/interfaces/definitions.ts
@@ -1,4 +1,4 @@
-export { default as dex } from './dex/definitions.js';
-export { default as primitives } from './primitives/definitions.js';
-export { default as support } from './support/definitions.js';
-export { default as runtime } from './runtime/definitions.js';
+export { default as dex } from './dex/definitions';
+export { default as primitives } from './primitives/definitions';
+export { default as support } from './support/definitions';
+export { default as runtime } from './runtime/definitions';
